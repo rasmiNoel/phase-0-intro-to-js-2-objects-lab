d78d06c8718a1ebfbe7c9d725b433fd82d4d06e3
rasmi <rasminoel@gmail.com>
2023-09-23 19:45:24 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Noel',
+    streetAddress: '568 Nairobi'
+}
+
+function updateEmployeeWithKeyAndValue() {
+    const nEmployee = {
+        ...employee,
+        name: 'Sam',
+        streetAddress: '11 Broadway'
+    };
+    return nEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const dEmployee = {
+        ...employee[key]
+    };
+    delete dEmployee[key];
+    return dEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
